59239d4579b49476283b67989ab1adb069bd86c0
shashicode2n4mcoffee <shahsicode2n4mcoffee@gmail.com>
2022-03-27 16:00:55 +0530

    Student page issue fixed

--- src/Components/StudentTitle.jsx
+++ src/Components/StudentTitle.jsx
@@ -4,30 +4,38 @@ import student1 from '../Assets/student1.svg'
 import experience1 from '../Assets/stud-acad.svg'
 import Download from './Download'
 
+import '../Styles/title.scss'
+
 const StudentTitle = () => {
   return (
-    <div className='stud-title ml-14 mt-10 flex justify-center flex-col items-start pl-10 relative '>
-      <div className='absolute top-[4rem]'>
-      <div>
+    <div
+      className=' mx-14 mt-10 flex justify-center flex-col items-start pl-10 relative title'
+      style={{
+        background: ' #15CD98',
+        width: '85%',
+      }}
+    >
+      <img
+        src={experience1}
+        alt=''
+        className='absolute right-[-8rem] bottom-[-2rem] title-chair'
+      />
+      <div className='title-msg'>
         <span className='p-1 text-[#ffffff] text-2xl font-bold'>
           Welcome to the
         </span>
         <span className='one text-2xl font-bold'>CoachEd Kingdom!</span>
+        <div className='p-4 pl-0 flex justify-start'>
+          <span className='p-1 text-[#ffffff] text-2xl font-bold'>
+            You are onto becoming a
+          </span>
+          <span className='two text-2xl font-bold'>CoachEd Warrior!</span>
+        </div>
       </div>
-      <div className='py-4'>
-        <span className='p-1 text-[#ffffff] text-2xl font-bold'>
-          You are onto becoming a
-        </span>
-        <span className='two text-2xl font-bold'>CoachEd Warrior!</span>
-      </div>
-      </div>
-      <div className='absolute bottom-[3rem]'>
+      <div className='absolute bottom-[2rem] left-[2rem]'>
         <img src={student1} alt='' className='w-[70%]' />
       </div>
-      <div className='absolute right-[-4rem] bottom-[-2.5rem] stud-acad'>
-        <img src={experience1} alt='' />
-      </div>
-      <Download/>
+      <Download />
     </div>
   )
 }
