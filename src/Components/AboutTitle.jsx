c69fb03ef0bc61547128132273c8871929815d77
shashicode2n4mcoffee <shahsicode2n4mcoffee@gmail.com>
2022-03-28 03:30:12 +0530

    About title issue solved

--- src/Components/AboutTitle.jsx
+++ src/Components/AboutTitle.jsx
@@ -6,19 +6,21 @@ const AboutTitle = () => {
   return (
     <div className='about-title ml-14 mt-10 flex justify-center flex-col items-start pl-10 relative '>
       <div className='absolute top-[4rem]'>
-        <div>
+        <div className='about-title-first'>
           <span className='p-1 text-[#ffffff] text-2xl font-light'>
             We are here to get a
           </span>
           <span className='text-white text-2xl font-bold'>
-            student their dream job{' '}
+            student their dream job
           </span>
         </div>
         <div className='py-4'>
-          <span className='p-1 text-[#ffffff] text-2xl font-light'>and</span>
-          <span className='text-white text-2xl font-bold'>
-            companies their perfect hire.{' '}
-          </span>
+          <div className='about-title-second'>
+            {/* <span className='p-1 text-[#ffffff] text-2xl font-light'></span> */}
+            <span className='text-white text-2xl font-bold'>
+              and companies their perfect hire.
+            </span>
+          </div>
         </div>
       </div>
       <div className='absolute right-[-4rem] bottom-[-2.5rem] about-acad'>
